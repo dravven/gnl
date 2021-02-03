ded84f533c512647eaa9de179a05a0a2fe7df748
dravven <danild98@gmail.com>
2021-02-03 13:07:53 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/untitled2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled2.dir/get_next_line.c.obj"
+  "CMakeFiles/untitled2.dir/get_next_line_utils.c.obj"
+  "libuntitled2.dll.a"
+  "untitled2.exe"
+  "untitled2.exe.manifest"
+  "untitled2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

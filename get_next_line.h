c637415a9c1b41991b140b87c060a9e4bcf6b276
dravven <danild98@gmail.com>
2021-02-07 13:21:45 +0300

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,13 @@
 # include <stdio.h>
 #include <fcntl.h>
 
-# define BUFF_SIZE 9
+//# define BUFFER_SIZE 10000000
 
 # define MALLCHECK(x) if (!x) return (-1);
 
 char        *ft_strcpy(char *dest, char *src);
 
 void		ft_bzero(void *b, size_t n);
-
-int	ft_strlen(char const *str);
 
 char		*ft_strjoin(char const *s1, char const *s2);
 
@@ -23,6 +21,8 @@ char        *ft_strchr(const char *str, int c);
 char	    *ft_strnew(size_t size);
 
 char	    *ft_strdup(char *src);
+
+int	ft_strlen(char const *str);
 
 int		    get_next_line(const int fd, char **line);
 #endif
